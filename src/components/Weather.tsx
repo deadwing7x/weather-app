7035428ab699e7fe83839a572e3f0d5dc03232ed
Anubhav Sarkar <anubhavsarkar7@gmail.com>
2021-04-03 13:54:17 +0530

    Added button to fetch forecast data for 5 days

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -9,11 +9,16 @@ const API_Key: string = "909490635a81cf7c920da17401bb60af";
 
 const Weather: React.FC<{}> = () => {
   const [place, setPlace] = useState<string>("");
-  const [weatherData, setWeatherData] = useState<IWeatherCardProps | any>();
-  const [forecastWeatherData, setForecastWeatherData] = useState<
-    IForecastWeatherProps | any
-  >();
-  const [loaded, setLoaded] = useState<boolean>(false);
+  const [weatherData, setWeatherData] = useState<IWeatherCardProps>();
+  const [
+    forecastWeatherData,
+    setForecastWeatherData,
+  ] = useState<IForecastWeatherProps>();
+  const [weatherDataLoaded, setWeatherDataLoaded] = useState<boolean>(false);
+  const [
+    forecastWeatherDataLoaded,
+    setForecastWeatherDataLoaded,
+  ] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string>("");
 
   const getCurrentWeatherForLocationSearched = async () => {
@@ -24,9 +29,9 @@ const Weather: React.FC<{}> = () => {
     return req;
   };
 
-  const getForecastData = async () => {
+  const getForecastData = async (location: string) => {
     const req = await fetch(
-      `${baseUrl}forecast?q=${place}&appid=${API_Key}&units=metric`
+      `${baseUrl}forecast?q=${location}&appid=${API_Key}&units=metric`
     );
 
     return req;
@@ -35,7 +40,6 @@ const Weather: React.FC<{}> = () => {
   const fetchCurrentWeather = async () => {
     if (place) {
       const currentWeatherResponse = await getCurrentWeatherForLocationSearched();
-      const forecastWeatherResponse = await getForecastData();
 
       currentWeatherResponse.json().then((data: IWeatherCardProps) => {
         const weatherCardProps: IWeatherCardProps = data;
@@ -43,30 +47,41 @@ const Weather: React.FC<{}> = () => {
         setWeatherData(weatherCardProps);
       });
 
+      if (currentWeatherResponse.status === 404) {
+        setErrorMessage(
+          "Couldn't fetch data for this city! Please search for some other city."
+        );
+        setPlace("");
+        setWeatherDataLoaded(false);
+      } else if (currentWeatherResponse.status === 200) {
+        setWeatherDataLoaded(true);
+        setPlace("");
+      }
+    } else {
+      setErrorMessage("Please enter a city to search!");
+    }
+  };
+
+  const fetchForecastWeatherData = async () => {
+    if (weatherData) {
+      const location: string = weatherData.name;
+
+      const forecastWeatherResponse = await getForecastData(location);
+
       forecastWeatherResponse.json().then((data: IForecastWeatherProps) => {
         const forecastWeatherProps: IForecastWeatherProps = data;
 
         setForecastWeatherData(forecastWeatherProps);
       });
 
-      if (
-        currentWeatherResponse.status === 404 ||
-        forecastWeatherResponse.status === 404
-      ) {
+      if (forecastWeatherResponse.status === 404) {
         setErrorMessage(
           "Couldn't fetch data for this city! Please search for some other city."
         );
-        setPlace("");
-        setLoaded(false);
-      } else if (
-        currentWeatherResponse.status === 200 ||
-        forecastWeatherResponse.status === 200
-      ) {
-        setLoaded(true);
-        setPlace("");
+        setForecastWeatherDataLoaded(false);
+      } else if (forecastWeatherResponse.status === 200) {
+        setForecastWeatherDataLoaded(true);
       }
-    } else {
-      setErrorMessage("Please enter a city to search!");
     }
   };
 
@@ -89,17 +104,43 @@ const Weather: React.FC<{}> = () => {
         />
       </div>
       <div>
-        {weatherData && forecastWeatherData ? (
-          loaded ? (
+        {weatherData ? (
+          weatherDataLoaded ? (
             <>
               <p className="flex justify-center">
                 Data fetched at{" "}
                 {new Date(weatherData.dt * 1000).toLocaleString()}
               </p>
               <WeatherCard {...weatherData} />
-              <hr />
-              <ForecastWeatherCard {...forecastWeatherData} />{" "}
+              {!forecastWeatherDataLoaded ? (
+                <>
+                  <div className="flex justify-center">
+                    <button
+                      className="mt-8 mb-4 outline-none rounded-md bg-blue-500 text-xl text-black-500 p-4"
+                      onClick={fetchForecastWeatherData}
+                    >
+                      Fetch next 5 days forecast data
+                    </button>
+                  </div>
+                  <hr />
+                </>
+              ) : (
+                <hr />
+              )}
             </>
+          ) : (
+            <p className="flex justify-center text-2xl text-red-600 font-bold">
+              {errorMessage}
+            </p>
+          )
+        ) : (
+          <p className="flex justify-center text-2xl text-red-600 font-bold">
+            {errorMessage}
+          </p>
+        )}
+        {forecastWeatherData ? (
+          forecastWeatherDataLoaded ? (
+            <ForecastWeatherCard {...forecastWeatherData} />
           ) : (
             <p className="flex justify-center text-2xl text-red-600 font-bold">
               {errorMessage}
